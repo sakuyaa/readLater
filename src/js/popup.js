75b541a22f02f735e5417157b9a1a7bbb87bbed3
sakuyaa <sakuyaa@users.noreply.github.com>
2023-09-19 15:04:55 +0800

    Use runtime.getURL instead of deprecated function

--- src/js/popup.js
+++ src/js/popup.js
@@ -6,7 +6,7 @@ let readLater = {
 			type: 'basic',
 			message: message + '',
 			title: browser.i18n.getMessage(title),
-			iconUrl: browser.extension.getURL('readLater.svg')
+			iconUrl: browser.runtime.getURL('readLater.svg')
 		});
 	},
 	removeData: (key, close) => {
